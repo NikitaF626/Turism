920388237460eb201039d13eacd1f4ca565928f9
nikit <nikit@NIKITAFE>
2024-12-20 19:06:32 +0700

    Добавьте файлы проекта.

---
+++ Turism/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using EO.WebBrowser;
+
+namespace Turism
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string htmlFilePath = @"C:/Users/nikit/source/repos/Turism/Turism/Map3.html";
+
+            // Проверьте, существует ли файл
+            if (File.Exists(htmlFilePath))
+            {
+                webView.Source = new Uri(htmlFilePath);
+            }
+            else
+            {
+                MessageBox.Show("HTML файл не найден: " + htmlFilePath);
+            }
+        }
+    }
+}
